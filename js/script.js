e91bf4342b5e2c1e63eb90a6bc98ace388317444
sallzzbr <evio.salgado@hotmail.com>
2018-03-18 14:35:38 -0300

    added new cats

--- js/script.js
+++ js/script.js
@@ -5,6 +5,21 @@ var cats = [
 		"clicks": 0
 	},
 	{
+		"name": "Jayme",
+		"pic": "imgs/Jayme.jpg",
+		"clicks": 0
+	},
+	{
+		"name": "Jorge",
+		"pic": "imgs/Jorge.jpg",
+		"clicks": 0
+	},
+	{
+		"name": "Juan",
+		"pic": "imgs/Juan.jpg",
+		"clicks": 0
+	},
+	{
 		"name": "Juarez",
 		"pic": "imgs/Juarez.jpg",
 		"clicks": 0
