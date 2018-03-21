1bf9154f878132f5f5e3b16a07c5e07dd279c364
sallzzbr <evio.salgado@hotmail.com>
2018-03-20 21:49:10 -0300

    pre-checkout commit

--- js/script.js
+++ js/script.js
@@ -25,21 +25,3 @@ var cats = [
 		"clicks": 0
 	}
 ];
-
-function makeCatRows() {
-	var toAdd = "";
-	$.each(cats, function(catIndex, cat) {
-		toAdd += "<div class='cat col-xs-6'><div class='container'><div class='name'>" + cat.name + "</div><img src='" + cat.pic + "' class='clickable'/><div id='" + catIndex.toString() + "' class='count'>" + cat.clicks.toString() + "</div></div></div>";
-    console.log(this, cat, catIndex);
-	});
-	$("#main").append("<div class='row'><div class='container'>" + toAdd + "</div></div>");
-}
-
-$(document).ready(function() {
-	makeCatRows();
-	$(".clickable").click(function(obj) {
-		var elem = obj.target.parentElement.childNodes[2];
-		cats[elem.id].clicks += 1;
-		$("#" + elem.id).text(cats[elem.id].clicks);
-	});
-});
